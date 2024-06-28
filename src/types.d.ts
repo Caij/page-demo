0b1b1df4ee931286f04612a16b5620b92d98557d
Caij <271945881@qq.com>
2024-06-28 17:29:18 +0800

    rollback 5

--- src/types.d.ts
+++ src/types.d.ts
@@ -213,10 +213,12 @@ export interface Form {
 }
 
 // WIDGETS
-export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' | 'classes'> {
+export interface Hero extends Omit<Headline, 'classes'>, Widget {
   content?: string;
-  actions?: string | CallToAction[];
   image?: string | unknown;
+  callToAction1?: CallToAction;
+  callToAction2?: CallToAction;
+  isReversed?: boolean;
 }
 
 export interface Team extends Omit<Headline, 'classes'>, Widget {
